732f2f301006aeb9840622dd98f02caf2c941795
Will Ostlund <wostlund@stuy.edu>
2015-12-15 17:52:35 -0500

    HW: Create a temperature converter

---
+++ hw/TempConverter.java
@@ -0,0 +1,10 @@
+public class TempConverter{
+
+    public static double CtoF(double temp){
+	return temp*(9/5)+32;
+    }
+
+    public static double FtoC(double temp){
+	return (temp-32)*5/9;
+    }
+}
