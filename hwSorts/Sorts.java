3e801d5c5ff72f5e3d7208fcd4f83d69e71cccb1
Will Ostlund <wostlund@stuy.edu>
2016-01-06 13:41:52 -0500

    Added a way of testing my sorts, unfortunately there's something wrong with my bubble sort

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -26,15 +26,32 @@ public class Sorts{
     }
 
     public static void bubbleSort(int[]jeff){
+	boolean frank = true;
 	for(int i=jeff.length; i>-1; i--){
 	    for(int k=0; k<i-1; k++){
 		if(jeff[k]>jeff[k+1]){
 		    int a = jeff[k];
 		    jeff[k]=jeff[k+1];
 		    jeff[k+1]=a;
+		    frank=false;
 		}
 	    }
+	    if(frank){
+		return;
+	    }
 	}
+    }
+
+    public static void fillRandom(int[]jeff){
+	for(int i=0; i<jeff.length; i++){
+	    jeff[i]=(int)(Math.random()*100000);
+	}
+    }
+
+    public static void swap(int[]jeff, int p, int q){
+	int a = jeff[p];
+	jeff[p]=jeff[q];
+	jeff[q]=a;
     }
     
     public static void main(String[]args){
